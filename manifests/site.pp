973543fa6772c06a1aae90fc3169d98657648cb4
tryoung1987 <timothy.young@ngc.com>
2017-02-22 07:31:10 -0700

    Update site.pp

--- manifests/site.pp
+++ manifests/site.pp
@@ -27,21 +27,20 @@ ini_setting { 'random ordering':
   setting => 'ordering',
   value   => 'title-hash',
 }
-
-# DEFAULT NODE
-# Node definitions in this file are merged with node data from the console. See
-# http://docs.puppetlabs.com/guides/language_guide.html#nodes for more on
-# node definitions.
-
-# The default node definition matches any node lacking a more specific node
-# definition. If there are no other nodes in this file, classes declared here
-# will be included in every node's catalog, *in addition* to any classes
-# specified in the console for that node.
-
 node default {
   # This is where you can declare classes for all nodes.
   # Example:
   #   class { 'my_class': }
   include role::classroom
   include examples::fundamentals
+  
+  # Exercise 7.1
+  file { 'motd':
+    ensure  => file,
+    owner   => 'root',
+    group   => 'root',
+    mode    => '0644',
+    path    => '/etc/motd',
+    content => "Think before you type\n",
+  }
 }

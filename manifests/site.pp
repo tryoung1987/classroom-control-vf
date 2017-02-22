f7d509974d1ed6d8e776f3c120a4e077be9d3e6e
tryoung1987 <timothy.young@ngc.com>
2017-02-22 08:52:12 -0700

    Update site.pp

--- manifests/site.pp
+++ manifests/site.pp
@@ -28,16 +28,6 @@ ini_setting { 'random ordering':
   value   => 'title-hash',
 }
 
-# DEFAULT NODE
-# Node definitions in this file are merged with node data from the console. See
-# http://docs.puppetlabs.com/guides/language_guide.html#nodes for more on
-# node definitions.
-
-# The default node definition matches any node lacking a more specific node
-# definition. If there are no other nodes in this file, classes declared here
-# will be included in every node's catalog, *in addition* to any classes
-# specified in the console for that node.
-
 node default {
   # This is where you can declare classes for all nodes.
   # Example:

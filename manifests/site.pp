9a9d5a9d2500a9fa5192e933a1cf1b25d2e4edb5
tryoung1987 <timothy.young@ngc.com>
2017-02-22 07:54:44 -0700

    Manage motd
    motd added

--- manifests/site.pp
+++ manifests/site.pp
@@ -39,9 +39,15 @@ ini_setting { 'random ordering':
 # specified in the console for that node.
 
 node default {
-  # This is where you can declare classes for all nodes.
-  # Example:
-  #   class { 'my_class': }
   include role::classroom
   include examples::fundamentals
+}
+
+file { 'motd':
+  ensure => file,
+  owner => 'root',
+  group => 'root',
+  mode => 'root',
+  path => '/etc/motd',
+  content => "Hello World\n",
 }
